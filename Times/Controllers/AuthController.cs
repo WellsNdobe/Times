3523f84731018d4fd7e100512dd00e17841772e7
Daniel Ndobe <danielndobe850@gmail.com>
2026-01-09 20:37:20 +0200

    Add project files.

---
+++ Times/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Times.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return Ok("Login successful");
+        }
+    }
+}
