8ddfebcd34d49e9b7d72c710ac1f36f98aeb71f6
Daniel Ndobe <danielndobe850@gmail.com>
2026-01-09 23:11:12 +0200

    Added Client Endpoints

---
+++ Times/Controllers/ClientController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Times.Contracts.Clients;
+using Times.Entities;
+using Times.Database;
+
+
+namespace Times.Controllers;
+
+[ApiController]
+[Route("api/clients")]
+[Authorize]
+public class ClientController : ControllerBase
+{
+	private readonly DataContext _db;
+
+	public ClientController(DataContext db)
+	{
+		_db = db;
+	}
+
+	private long UserId =>
+		long.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+	[HttpGet]
+	public IActionResult GetMyClients()
+	{
+		var clients = _db.Clients
+			.Where(c => c.UserId == UserId)
+			.Select(c => new ClientResponse
+			{
+				Id = c.Id,
+				Name = c.Name,
+				Email = c.Email,
+				Phone = c.Phone
+			})
+			.ToList();
+
+		return Ok(clients);
+	}
+
+	[HttpPost]
+	public IActionResult Create(CreateClientRequest request)
+	{
+		var client = new Client
+		{
+			Name = request.Name,
+			Email = request.Email,
+			Phone = request.Phone,
+			UserId = UserId
+		};
+
+		_db.Clients.Add(client);
+		_db.SaveChanges();
+
+		var response = new ClientResponse
+		{
+			Id = client.Id,
+			Name = client.Name,
+			Email = client.Email,
+			Phone = client.Phone
+		};
+
+		return CreatedAtAction(nameof(GetMyClients), response);
+	}
+}
---
+++ Times/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Times.Database;
+
+
+namespace Times.Controllers
+{
+	[ApiController]
+	public class RootController : ControllerBase
+	{
+		private readonly DataContext _db;
+
+		public RootController(DataContext db)
+		{
+			_db = db;
+		}
+
+		[HttpGet("/")]
+		public async Task<IActionResult> Get()
+		{
+			var dbOnline = await _db.Database.CanConnectAsync();
+
+			return Ok(new
+			{
+				service = "TimeSheet API",
+				status = "Running",
+				database = dbOnline ? "Connected" : "Unavailable",
+				timestamp = DateTime.UtcNow
+			});
+		}
+	}
+
+
+	[Authorize]
+	[ApiController]
+	[Route("api/secure")]
+	public class SecureController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return Ok("You are authenticated");
+		}
+	}
+
+}
